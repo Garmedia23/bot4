187ef00d91f38385f2097c352f95d5d8c39fef20
Garmedia23 <104086584+Garmedia23@users.noreply.github.com>
2022-04-21 12:37:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -244,7 +244,7 @@ def onmessage(update,bot:ObigramClient):
         tl_admin_user = os.environ.get('tl_admin_user')
 
         #set in debug
-        tl_admin_user = 'rockstar984'
+        tl_admin_user = 'Inserteaki'
 
         jdb = JsonDatabase('database')
         jdb.check_create()
@@ -562,7 +562,7 @@ def main():
     bot_token = os.environ.get('bot_token')
 
     #set in debug
-    bot_token = '5267170951:AAEqx23n7KRgx_-qey41Fhw_VnxWf2IG9iU'
+    bot_token = '5374348379:AAH0_1EHqAFPBJVIUmlm_Yo9JUKHAPTJuFM'
 
     bot = ObigramClient(bot_token)
     bot.onMessage(onmessage)
